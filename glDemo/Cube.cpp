bb495a7d5f70eefe45eff61d293c45d7daf22712
goatbird2 <st20319024@outlook.cardiffmet.ac.uk>
2025-05-02 17:52:47 +0100

    S1_colourupdatecube_1
    - i changed some colours to make sure its rendering right

--- glDemo/Cube.cpp
+++ glDemo/Cube.cpp
@@ -24,14 +24,14 @@ static float positionArray[] = {
 // Packed colour buffer for principle axes model
 static float colourArray[] = {
 
+	1.0f, 0.0f, 0.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
 	0.0f, 1.0f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
-	0.0f, 1.0f, 0.0f, 1.0f,
+	1.0f, 1.0f, 0.0f, 0.0f,
 
 	0.0f, 0.0f, 1.0f, 1.0f,
 	0.0f, 1.0f, 1.0f, 1.0f,
-	0.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 1.0f, 1.0f,
 	0.0f, 0.0f, 0.0f, 1.0f
 };
 
