e97638e2e369c3ac8af9d7cf127cff6c849a801a
goatbird2 <st20319024@outlook.cardiffmet.ac.uk>
2025-05-04 19:45:11 +0100

    S2_cubegameobjectfiles_edits_1
    - includes
    - header public
    - cpp structure

---
+++ glDemo/CubeGameObject.cpp
@@ -0,0 +1,14 @@
+
+// ET: "#pragma once" prevents double inclusion of files
+#pragma once
+
+// ET: This header file inherits from the GameObject headerfile
+#include "GameObject.h"
+
+class CubeGameObject : public GameObject {
+public:
+    // ET: Constructor to create the cube
+    CubeGameObject();
+    // ET: Deconstructor 
+    ~CubeGameObject();
+};
