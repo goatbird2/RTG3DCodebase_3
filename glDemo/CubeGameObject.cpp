aa579c9a78f8e7462837a7f87eedf6b0503142d9
goatbird2 <st20319024@outlook.cardiffmet.ac.uk>
2025-05-04 22:32:52 +0100

    S2_cubegameobjectfiles_edits_3
    - first draft of filling oout CubeGameObject cpp/h

--- glDemo/CubeGameObject.cpp
+++ glDemo/CubeGameObject.cpp
@@ -1,14 +1,33 @@
 
 // ET: "#pragma once" prevents double inclusion of files
-#pragma once
+#include ""
 
 // ET: This header file inherits from the GameObject headerfile
 #include "GameObject.h"
 
+// ET: We need the definition of the cube
+#include "core/cube.h"
+
+
 class CubeGameObject : public GameObject {
 public:
-    // ET: Constructor to create the cube
-    CubeGameObject();
-    // ET: Deconstructor 
+    // ET: Constructor (learned what that is in cube.cpp)
+    CubeGameObject::CubeGameObject() {
+        // ET: We create a Mesh-object (here, the cube)
+        mesh = new Cube();
+
+        // ET: Assigns mesh to GameObject
+        SetMesh(mesh);
+
+        // ET: Sets position of cube in the room (here: middle)
+        SetPosition(glm::vec3(0.0f, 0.0f, 0.0f));
+
+        // ET: Sets scale of cube (here: standard size)
+        SetScale(glm::vec3(1.0f));
+
+        // ET: Sets rotation (here: resets it to 0)
+        SetRotation(glm::vec3(0.0f));
+    }
+    // ET: Deconstructor (learned what that is in cube.cpp)
     ~CubeGameObject();
 };
--- glDemo/CubeGameObject.h
+++ glDemo/CubeGameObject.h
@@ -2,13 +2,20 @@
 // ET: "#pragma once" prevents double inclusion of files
 #pragma once
 
-// ET: This header file inherits from the GameObject headerfile
+// ET: This header file inherits transformations etc. from the GameObject headerfile
 #include "GameObject.h"
 
+// ET: Forward declaration: We are telling the complier that a class "cube" exists (doesnt load it yet tho)
+// ET: We hereby avoid including cube.h directly -> saves compilation time
+class Cube;
+
+// ET: Inheritance-> CubeGameObject is a GameObject (enables acess to SetPosition, SetRotation, etc.)
 class CubeGameObject : public GameObject {
 public:
     // ET: Constructor to create the cube
     CubeGameObject();
-    // Et: Deconstructor to 
+    // ET: Deconstructor
     ~CubeGameObject();
+    // ET: Overrides logic per frame to for example. rotate it
+    void Update(float deltaTIME) override;
 };
